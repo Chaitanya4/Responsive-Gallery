3dec20f2e9365956fa57e477198ad930a4fbbe65
Chaitanya4 <31300719+Chaitanya4@users.noreply.github.com>
2019-02-23 08:28:24 +0530

    Add files via upload

---
+++ contactus/validation.js
@@ -0,0 +1,51 @@
+function validate()
+{
+	var name = document.forms["contactus"]["name"];               
+    var email = document.forms["contactus"]["email"];    
+    var phone = document.forms["contactus"]["phone"];  
+    var message =  document.forms["contactus"]["message"];  
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your mobile number."); 
+        phone.focus(); 
+        return false; 
+    } 
+    if (message.value == "")                                   
+    { 
+        window.alert("Please enter a message"); 
+        email.focus(); 
+        return false; 
+    } 
+  
+   
+    return true; 
+}
